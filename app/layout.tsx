252983c0b2ea4545b66d87951e2e5cb98ff621aa
Jooseulgi <iauuai21@gmail.com>
2023-08-04 12:57:06 +0900

    feat: :construction: Navbar 작업 중

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
+import Navbar from './components/Navbar/Navbar'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar/>
         {children}
       </body>
     </html>
